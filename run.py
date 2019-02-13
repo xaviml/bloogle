95c6a3c9fb70f35524bf6337785b8d121b046c61
Albert Folch <albertfolch58@hotmail.com>
2019-02-13 11:34:47 +0100

    gizmodo spider added

--- run.py
+++ run.py
@@ -3,9 +3,11 @@ from scrapy.crawler import CrawlerProcess
 from bloogle.spiders.wired_spider import WiredSpider
 from bloogle.spiders.medium_spider import MediumSpider
 from bloogle.spiders.steemit_spider import SteemitSpider
+from bloogle.spiders.gizmodo_spider import GizmodoSpider
 
 process = CrawlerProcess()
-process.crawl(WiredSpider)
-process.crawl(MediumSpider)
-process.crawl(SteemitSpider)
+#process.crawl(WiredSpider)
+#process.crawl(MediumSpider)
+#process.crawl(SteemitSpider)
+process.crawl(GizmodoSpider)
 process.start() # the script will block here until all crawling jobs are finished
