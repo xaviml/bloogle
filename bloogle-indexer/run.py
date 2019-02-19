9ee3a6fc431809b74c8a2c50a3f22bc314be4d69
xaviml <xaviml.93@gmail.com>
2019-02-19 17:26:25 +0100

    Calling to elasticsearch

--- bloogle-indexer/run.py
+++ bloogle-indexer/run.py
@@ -1,12 +1,46 @@
 import optparse
+from indexer.elasticsearch_connector import ElasticsearchConnector
+from indexer.parser import HTMLparser
+import glob
+import os
 
 parser = optparse.OptionParser()
 
-parser.add_option('-i', '--input',
-    action="collect", dest="input",
-    help="Input path")
+parser.add_option('-i', '--input', dest="input", help="Input path")
 
 options, args = parser.parse_args()
 #options.input -> to access the data folder
 
+
+def read_links(filepath):
+    output = {}
+    with open(filepath) as f:
+        lines = f.readlines()
+        for line in lines:
+            l = line.split('\t')
+            output[l[0]] = {
+                'url': l[1],
+                'links': l[2:]
+            }
+    return output  
+
+connector = ElasticsearchConnector()
+
 # for loop through htmls file, calling the parser and elasticsearch
+path = os.path.join(options.input, '*')
+folders = glob.glob(path)
+for folder in folders:
+    linkspath =  os.path.join(folder, 'links.txt')
+    path = os.path.join(folder, 'page', '*')
+    filepaths = glob.glob(path)
+    files_info = read_links(linkspath)
+    for filepath in filepaths:
+        with open(filepath, 'r') as f:
+            filename = filepath.split(os.path.sep)[-1]
+            parsed_content = HTMLparser(f.read())
+            connector.store(
+                title = parsed_content['title'],
+                content = parsed_content['content'],
+                url = files_info[filename]['url'],
+                blog = filepath.split(os.path.sep)[-3]
+            )
