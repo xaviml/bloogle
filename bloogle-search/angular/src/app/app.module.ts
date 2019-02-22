56ed735577cfd49a1a1921ed14ee539c1b2def93
Albert Folch <albertfolch58@hotmail.com>
2019-02-22 19:24:32 +0100

    angular skeleton

---
+++ bloogle-search/angular/src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatStepperModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatButtonToggleModule,
+  MatSliderModule
+} from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+const modules = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatDatepickerModule,
+  MatMomentDateModule,
+  FormsModule,
+  ReactiveFormsModule,
+  LayoutModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatListModule,
+  MatSliderModule,
+  MatStepperModule,
+  MatPaginatorModule,
+  HttpClientModule,
+];
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app/app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ...modules
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
