2fc50cfea9f5da2e8b228ce6a2e733e12774aa31
Albert Folch <albertfolch58@hotmail.com>
2019-03-14 22:43:38 +0100

    fixed timeout

--- bloogle-indexer/delete-deleted-posts.py
+++ bloogle-indexer/delete-deleted-posts.py
@@ -1,12 +1,13 @@
 import requests
 from elasticsearch_dsl import connections
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers as h
 from indexer.post import Post
 from elasticsearch_dsl import Search
 
 def isPostOnline(url):
     r = requests.head(url)
-    return  r.status_code >= 200 and r.status_code < 300
+    return  r.status_code >= 200 and r.status_code < 400, r.status_code
 
 def deletePost(url):
     s = Search(index='blog').query('match', _id=url)
@@ -16,16 +17,26 @@ def deletePost(url):
 client = Elasticsearch()
 connections.create_connection(hosts=['localhost'], port=9200)
 
-search = Search(using=client) # search all blogs
+search = Search(using=client).params(request_timeout=999999) # search all blogs
 
 # Count search results
 total = search.count()
 print('total indexed blogs', total)
 
+i = 0
 deletedBlogs = 0
-for post in search.scan():
-    if not isPostOnline(post.url):
-        post.delete()
-        deletedBlogs += 1
+#for post in search.scan():
+for d_post in h.scan(client, scroll=u'24h'):
+        post = d_post['_source']
+        url = post['url']
+        isOnline, httpCode = isPostOnline(url)
+        if not isOnline:
+                deletePost(url)
+                deletedBlogs += 1
+                print('code', httpCode, 'deleted:', url)
+        if i % 1000 == 0:
+                print('i = ', i)
+        i+=1
+
 
 print('deletedBlogs', deletedBlogs)
