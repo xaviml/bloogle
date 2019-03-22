1b9185f1f7c2f2220bb1ef17bae748ef9dc62812
xaviml <xaviml.93@gmail.com>
2019-03-22 22:41:11 +0100

    Adding a decorator to check the input of the functions

--- evaluation/calculate_metrics.py
+++ evaluation/calculate_metrics.py
@@ -4,40 +4,45 @@ import itertools
 import numpy as np
 import os
 import math
+import pandas as pd
 
+def checker(func):
+    def wrapper(rank, *args, **kwargs):
+        return 0 if sum(rank) == 0 else func(rank, *args, **kwargs)
+    return wrapper
 
+@checker
 def calc_precission_at_k(rank, k):
     return sum(rank[:k])/k
 
-
+@checker
 def calc_precision(rank):
     return calc_precission_at_k(rank, len(rank))
 
-
+@checker
 def calc_average_precision(rank):
     precisions_at_k = [calc_precission_at_k(rank, idx+1) for idx, value in enumerate(rank) if value]
-    return sum(precisions_at_k) / len(precisions_at_k) if len(precisions_at_k) != 0 else 0
+    return sum(precisions_at_k) / len(precisions_at_k)
 
-
+@checker
 def calc_reciprocal_rank(rank):
     return next((1/(idx+1) for idx, value in enumerate(rank) if value), 0)
 
-
+@checker
 def calc_DCG(rank):
     # We are adding 2 in the denominator because the idx is 0-based index, so we need to add an extra 1
     return sum((math.pow(2, value)-1)/(math.log2(idx+2)) for idx, value in enumerate(rank))
 
-
+@checker
 def calc_normalized_DCG(rank):
     ideal_rank = sorted(rank, reverse=True)
     DCG = calc_DCG(rank)
     IDCG = calc_DCG(ideal_rank)
-    return DCG/IDCG if IDCG != 0 else 0
+    return DCG/IDCG
 
-"""Expected reciprocal rank"""
+@checker
 def calc_err(rank, theta=1):
-    if max(rank) == 0:
-        return 0
+    """Expected reciprocal rank"""
     rank = list(map(lambda r: r/max(rank), rank))
     err = 0
     for k, R_k in enumerate(rank, 1):
@@ -47,20 +52,37 @@ def calc_err(rank, theta=1):
         err += 1/k * p * R_k * (theta**(k-1))
 
     return err
+
 if __name__ == "__main__":
     with open('validations/global.json') as f:
         data = json.load(f)
     
+    df = []
     queries = data['queries']
     for query in queries:
         relevancy = [elem['relevant'] for elem in query['data']]
+        precision = calc_precision(relevancy)
+        precission_at_2 = calc_precission_at_k(relevancy, 2)
+        precission_at_5 = calc_precission_at_k(relevancy, 5)
+        reciprocal_rank = calc_reciprocal_rank(relevancy)
+        average_precision = calc_average_precision(relevancy)
+        normalized_DCG = calc_normalized_DCG(relevancy)
+        item = {
+            'query': query['query'],
+            'precission': precision,
+            'precission at rank 2': precission_at_2,
+            'precission at rank 5': precission_at_5,
+            'reciprocal rank': reciprocal_rank,
+            'average precision': average_precision,
+            'normalized DCG': normalized_DCG
+        }
+        df.append(item)
         print(f'Query: {query["query"]}')
-        print(f'Precision: {calc_precision(relevancy):.3f}')
-        print(f'Precision at 2: {calc_precission_at_k(relevancy, 2):.3f}')
-        print(f'Precision at 5: {calc_precission_at_k(relevancy, 5):.3f}')
-        print(f'Reciprocal rank: {calc_reciprocal_rank(relevancy):.3f}')
-        print(f'Average precision (AP): {calc_average_precision(relevancy):.3f}')
-        #print(f'DCG: {calc_DCG(relevancy):.3f}')
-        print(f'Normalized DCG: {calc_normalized_DCG(relevancy):.3f}')
-        #print(f'Expected Reciprocal Rank: {calc_err(relevancy, 1):.3f}')
+        print(f'Precision: {precision:.3f}')
+        print(f'Precision at 2: {precission_at_2:.3f}')
+        print(f'Precision at 5: {precission_at_5:.3f}')
+        print(f'Reciprocal rank: {reciprocal_rank:.3f}')
+        print(f'Average precision (AP): {average_precision:.3f}')
+        print(f'Normalized DCG: {normalized_DCG:.3f}')
         print()
+    
